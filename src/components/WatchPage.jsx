df973eb091099ad1ad1efa8a37ac559fbe823719
xSaadRM <saadrm.dev@gmail.com>
2024-04-03 01:53:44 +0000

    fixed bugs

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -6,6 +6,7 @@ import React from "react";
 import VideoJS from "./HLSPlayer";
 import videojs from "video.js";
 import DisableDevtool from "disable-devtool";
+import ScaleLoader from "react-spinners/ScaleLoader";
 
 const WatchPage = () => {
   const location = useLocation();
@@ -21,6 +22,8 @@ const WatchPage = () => {
   const [sources, setSources] = useState([]);
   const [streamUrl, setStreamURL] = useState("");
   const [movieNotFound, setMovieNotFound] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const handleServerSwitch = async (serverID) => {
     const response = await fetch(
       `https://vidsrc.to/ajax/embed/source/${serverID}`
@@ -37,7 +40,6 @@ const WatchPage = () => {
   }, []);
   useEffect(() => {
     const getSlug = async () => {
-
       // Manual encoding for characters that aren't encoded in the standard encodeURIComponent function
       const manualEncode = (str) => {
         return Array.from(str)
@@ -89,12 +91,15 @@ const WatchPage = () => {
           }
         );
         const data = await response.json();
+        let found = false;
         for (const item of data.data.items) {
           if (item.contentable.tmdbId == hrefDetails[1]) {
             setSlug(item.slug);
-          } else {
-            setMovieNotFound(true);
+            found = true;
           }
+        }
+        if (!found) {
+          setMovieNotFound(true);
         }
       } catch (error) {
         console.error("Error fetching", error);
@@ -163,42 +168,45 @@ const WatchPage = () => {
 
   useEffect(() => {
     const getIFrame = async () => {
-      const response = await fetch(
-        `https://rough.isra.workers.dev/?destination=https%3A%2F%2Fridomovies.tv%2Fcore%2Fapi%2Fepisodes%2F${
-          dataIDs[hrefDetails[3] - 1].id
-        }%2Fvideos`,
-        {
-          headers: {
-            accept: "*/*",
-            "accept-language": "en-US,en;q=0.9",
-            "sec-ch-ua":
-              '"Microsoft Edge";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "cross-site",
-          },
-          referrerPolicy: "no-referrer",
-          body: null,
-          method: "GET",
-        }
-      );
-      const data = await response.json();
-      const dataIFrameSrc = data.data[0].url.match(/data-src="([^"]+)"/);
+      if (dataIDs.length > 0 && hrefDetails[3] && dataIDs[hrefDetails[3] - 1]) {
+        const episodeId = dataIDs[hrefDetails[3] - 1].id;
+        const response = await fetch(
+          `https://rough.isra.workers.dev/?destination=https%3A%2F%2Fridomovies.tv%2Fcore%2Fapi%2Fepisodes%2F${episodeId}%2Fvideos`,
+          {
+            headers: {
+              accept: "*/*",
+              "accept-language": "en-US,en;q=0.9",
+              "sec-ch-ua":
+                '"Microsoft Edge";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+              "sec-ch-ua-mobile": "?0",
+              "sec-ch-ua-platform": '"Windows"',
+              "sec-fetch-dest": "empty",
+              "sec-fetch-mode": "cors",
+              "sec-fetch-site": "cross-site",
+            },
+            referrerPolicy: "no-referrer",
+            body: null,
+            method: "GET",
+          }
+        );
+        const data = await response.json();
+        const dataIFrameSrc = data.data[0].url.match(/data-src="([^"]+)"/);
 
-      if (dataIFrameSrc) {
-        const dataSrcValue = dataIFrameSrc[1];
-        setIFrameUrl(dataSrcValue);
+        if (dataIFrameSrc) {
+          const dataSrcValue = dataIFrameSrc[1];
+          setIFrameUrl(dataSrcValue);
+        } else {
+          console.log("No data-src attribute found in the URL.");
+        }
       } else {
-        console.log("No data-src attribute found in the URL.");
+        // Handle the case where dataIDs or episodeId is not available
       }
     };
 
     if (dataIDs) {
       getIFrame();
     }
-  }, [dataIDs]);
+  }, [hrefDetails, dataIDs]);
 
   useEffect(() => {
     const getM3U8 = async () => {
@@ -290,9 +298,19 @@ const WatchPage = () => {
             <div className="player-container">
               <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
             </div>
-          ) : (
+          ) : movieNotFound === true ? (
             <div className="movie-not-found">
               <p>this ressource is not found on any server</p>
+            </div>
+          ) : (
+            <div className="loading-animation">
+              <ScaleLoader
+                color="blue"
+                loading={!movieNotFound || !streamUrl}
+                size={150}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+              />
             </div>
           )}
         </div>
