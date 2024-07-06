62ded9906840830efe6df7294f356888714e05a8
xSaadRM <saadrm.dev@gmail.com>
2024-07-06 22:48:51 +0100

    Add: header

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import "./App.css";
+import logo from "./assets/logo.png";
+import NightsStayIcon from "@suid/icons-material/NightsStay";
+import LightMode from "@suid/icons-material/LightMode";
+import MenuSharp from "./Components/MenuSharp";
+
+const toggleMenu = (event) => {
+  const menuSharp = event.target.closest(".MenuSharpIcon");
+
+  if (menuSharp) {
+    menuSharp.classList.toggle("active");
+  }
+};
+function App() {
+  return (
+    <div>
+      <header class="flex">
+        <div class="brand flex">
+          <div class="siteLogo">
+            <img src={logo} />
+          </div>
+          <p class="siteName">MovieBreak</p>
+        </div>
+        <div class="headerLeftSide flex">
+          <div
+            class="themeSwitcher glow"
+            onclick={() => {
+              const nextTheme =
+                document.documentElement.getAttribute("data-theme") === "dark"
+                  ? "light"
+                  : "dark";
+              document.documentElement.setAttribute("data-theme", nextTheme);
+            }}
+          >
+            <LightMode class="lightButton" />
+            <NightsStayIcon class="darkButton" />
+          </div>
+          <div className="leftMenuWrapper flex">
+            <MenuSharp class="MenuSharpIcon" onclick={toggleMenu} />
+            <div class="leftMenu flex">
+              <a>about</a>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/MenuSharp.jsx
@@ -0,0 +1,13 @@
+import $ from "@suid/icons-material/utils/createSvgIcon";
+
+export default $(
+  () => (
+    <>
+      <path d="M3 6h18v2H3v2z" />
+      <path d="M3 13h18v-2H3v2z" />
+      <path d="M3 13h18v-2H3v2z" />
+      <path d="M3 18h18v-2H3v2z" />
+    </>
+  ),
+  "MenuSharp"
+);
