58acbd57afae8060680abeecea0c69ce20070d70
xSaadRM <saadrm.dev@gmail.com>
2024-07-08 16:41:21 +0100

    Fix: skeleton loading size when image failed loading

--- src/Components/LazyImage.jsx
+++ src/Components/LazyImage.jsx
@@ -4,12 +4,16 @@ const LazyImage = (props) => {
   return (
     <div class="lazyImage flex">
       <img
+        onerror={(e) => e.target.classList.remove("show")}
         {...props}
-        onload={(e) => {
-          e.target.classList.add("show");
-        }}
+        onload={(e) => e.target.classList.add("show")}
       ></img>
-      <Skeleton  variant="rectangular" animation={"wave"} class="skeleton" style={{ "aspect-ratio": props.ratio }} />
+      <Skeleton
+        variant="rectangular"
+        animation={"wave"}
+        class="skeleton"
+        style={{ "aspect-ratio": props.ratio }}
+      />
     </div>
   );
 };
