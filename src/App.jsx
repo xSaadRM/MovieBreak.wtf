a239420c15f7f1f26a242c800a2ccf9891ebeab6
xSaadRM <saadrm.dev@gmail.com>
2024-07-10 13:15:07 +0100

    Fix: Header isn't sticky

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Home from "./Components/Home";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <Home />
-    </div>
+    </>
   );
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -14,41 +14,39 @@ const Header = () => {
   };
 
   return (
-    <div>
-      <header class="flex">
-        <div class="brand flex">
-          <div class="siteLogo">
-            <img src={logo} />
-          </div>
-          <p class="siteName">MovieBreak</p>
+    <header class="flex">
+      <div class="brand flex">
+        <div class="siteLogo">
+          <img src={logo} />
         </div>
-        <div class="headerLeftSide flex">
-          <div
-            class="themeSwitcher glow"
-            onclick={() => {
-              const nextTheme =
-                document.documentElement.getAttribute("data-theme") === "dark"
-                  ? "light"
-                  : "dark";
-              document.documentElement.setAttribute("data-theme", nextTheme);
-            }}
-          >
-            <LightMode class="lightButton" />
-            <NightsStayIcon class="darkButton" />
-          </div>
-          <div className="leftMenuWrapper flex">
-            <MenuSharp class="MenuSharpIcon" onclick={toggleMenu} />
-            <div class="anim2 leftMenu flex glowContain">
-              <a>Home</a>
-              <a>TV Shows</a>
-              <a>Movies</a>
-              <a>Watchlist</a>
-              <a>History</a>
-            </div>
+        <p class="siteName">MovieBreak</p>
+      </div>
+      <div class="headerLeftSide flex">
+        <div
+          class="themeSwitcher glow"
+          onclick={() => {
+            const nextTheme =
+              document.documentElement.getAttribute("data-theme") === "dark"
+                ? "light"
+                : "dark";
+            document.documentElement.setAttribute("data-theme", nextTheme);
+          }}
+        >
+          <LightMode class="lightButton" />
+          <NightsStayIcon class="darkButton" />
+        </div>
+        <div className="leftMenuWrapper flex">
+          <MenuSharp class="MenuSharpIcon" onclick={toggleMenu} />
+          <div class="anim2 leftMenu flex glowContain">
+            <a>Home</a>
+            <a>TV Shows</a>
+            <a>Movies</a>
+            <a>Watchlist</a>
+            <a>History</a>
           </div>
         </div>
-      </header>
-    </div>
+      </div>
+    </header>
   );
 };
 
